db72a17bd2b7eb5f69ba03c2c2d1782cd39f1006
Tomohiro Yamashina <110611464+Tomohiro-Yamashina@users.noreply.github.com>
2024-03-13 23:57:56 +0900

    変更

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,20 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/sample-site/',
+	base: './',
+	publicDir: 'public',
+	build: {
+		rollupOptions: {
+			output: {
+				assetFileNames: ({ name }) => {
+					if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? '')) {
+						return 'assets/images/[name]-[hash][extname]';
+					}
+					return 'assets/[ext]/[name]-[hash][extname]';
+				},
+			},
+		},
+	},
 	css: {
 		devSourcemap: true,
 	},
